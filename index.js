6770cc402b5cb86275af4af9a97314b269d0a837
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-17 12:45:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your classes here
+
+class Tree{
+  constructor(species){
+    this.species=species
+  this.definition="A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+    }
+   static definition(){
+      return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+    }
+}
+
+class Deciduous extends Tree{
+  constructor(definition,name) {
+        super(definition);
+        this.name=name
+    }
+  static definition(){
+      return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Deciduous trees shed their leaves annually."
+    }
+}
+
+
+class Evergreen extends Tree{
+  constructor(definition,name) {
+        super(definition);
+        this.name=name
+    }
+  static definition(){
+      return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Evergreens keep their leaves all year round."
+    }
+}
